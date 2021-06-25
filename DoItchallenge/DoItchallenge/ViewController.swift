85321e43f75cb6be985ce30cfca2a38721a8656e
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-06-25 12:29:16 +0900

    DoIt 도전문제
    두잇 도전문제

--- DoItchallenge/DoItchallenge/ViewController.swift
+++ DoItchallenge/DoItchallenge/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var imgNum = 0
 
+class ViewController: UIViewController {
+    
+    
+    
+    var imgName = ["1.png","2.png", "3.png", "4.png", "5.png", "6.png"]
+
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: imgName[0])
     }
 
-
+    @IBAction func btnPrev(_ sender: UIButton) {
+        imgNum = imgNum - 1
+        
+        if (imgNum < 0){
+            imgNum = imgName.count - 1
+        }
+        imgView.image = UIImage(named: imgName[imgNum])
+    }
+    
+    @IBAction func btnNext(_ sender: UIButton) {
+        imgNum = imgNum + 1
+        
+        if (imgNum >= imgName.count){
+            imgNum = 0
+        }
+        imgView.image = UIImage(named: imgName[imgNum])
+    }
 }
 
