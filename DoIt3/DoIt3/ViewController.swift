694520738463a90c24050518c0e42d2eab1ec1c9
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-06-25 10:50:54 +0900

    DoIt 3강
    이미지 뷰 앱

--- DoIt3/DoIt3/ViewController.swift
+++ DoIt3/DoIt3/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isZoom = false //확대여부를 나타내는 함수
+    var imgOn: UIImage? //켜진 전구 이미지가 있는 UIImage 타입의 함수
+    var imgOff: UIImage? //꺼진 전구 이미지가 있는 UIImage 타입의 함수
+    
 
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
     }
 
-
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if(isZoom){
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+        
+    }
+    
+    @IBAction func switchImageOff(_ sender: UISwitch) {
+        
+        if sender.isOn{
+            imgView.image = imgOn
+        }
+        else{
+            imgView.image = imgOff
+        }
+        
+    }
+    
 }
 
