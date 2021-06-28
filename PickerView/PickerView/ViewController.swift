0356c391c1bdcb4dc85c7328567bb186ba3162be
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-06-28 17:08:42 +0900

    Do It 5강
    Do It 5강 피커뷰로 이미지 선택하기

---
+++ PickerView/PickerView/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by GSM04 on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT: CGFloat = 80
+    var imageArrary = [UIImage?]()
+    var imageFileName = ["1.jpg", "2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
+    
+    @IBOutlet var pickerImage: UIPickerView!
+    @IBOutlet var lblImageFileName: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 0 ..< MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArrary.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArrary[0]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArrary[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int ) {
+        
+        lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArrary[row]
+    }
+    
+}
+
