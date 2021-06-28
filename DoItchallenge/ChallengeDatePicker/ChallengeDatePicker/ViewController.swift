e41c77ab9cb6c0d379ff88e49e95dd4c02526793
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-06-28 09:55:39 +0900

    Do It 4강 도전문제
    데이트피커를 이용하여 알람시계 만들기

--- DoItchallenge/ChallengeDatePicker/ChallengeDatePicker/ViewController.swift
+++ DoItchallenge/ChallengeDatePicker/ChallengeDatePicker/ViewController.swift
@@ -1,19 +1,50 @@
-//
-//  ViewController.swift
-//  ChallengeDatePicker
-//
-//  Created by GSM04 on 2021/06/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    var alarm: String?
+    var current: String?
+    
+    @IBOutlet var lblCrtTime: UILabel!
+    @IBOutlet var lblAlarmTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func selectTime(_ sender: UIDatePicker) {
+        let selectTime = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        
+        lblAlarmTime.text = "선택시간:" + formatter.string(from: selectTime.date)
+        
+        alarm = formatter.string(from: selectTime.date)
+    }
+    
+    @objc func updateTime(){
+        
+        let date = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblCrtTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        current = formatter.string(from: date as Date)
+        
+        if (alarm == current){
+            view.backgroundColor = UIColor.red
+        }
+        else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
